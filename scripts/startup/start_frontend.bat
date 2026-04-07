91935f2ec94238eabcba005f520d48c568fd54e1
n0body-byte <480427503@qq.com>
2026-04-07 14:05:42 +0800

    fix(scripts): correct frontend startup working directory

--- scripts/startup/start_frontend.bat
+++ scripts/startup/start_frontend.bat
@@ -4,6 +4,6 @@ setlocal
 set "SCRIPT_DIR=%~dp0"
 for %%I in ("%SCRIPT_DIR%..\..") do set "ROOT_DIR=%%~fI"
 
-cd /d "%ROOT_DIR%frontend"
+cd /d "%ROOT_DIR%\frontend"
 echo [INFO] Starting frontend on http://127.0.0.1:5173
 npm.cmd run dev
